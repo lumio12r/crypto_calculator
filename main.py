3bd36fb738409eb39b513e3d6a2a7c16398746fd
lucas <lu.zatonski34@gmail.com>
2021-03-05 19:38:22 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+amount = int(input())
+cost_per_one = 688
+cost_per_all = amount * cost_per_one
+print("I have {} litecoin.".format(amount))
+print("{} litecoins cost {} dollars".format(amount, cost_per_all))
+print("I am rich! Yippee!")
