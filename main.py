3e35266c5b72af4b68fd5d98a568d4416c9a890f
lucas <lu.zatonski34@gmail.com>
2021-03-05 20:38:14 +0100

    Litecoin in other currencies

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-amount = int(input('Please, enter the number of litecoins you have: '))
-cost_per_one = float(input('Please, enter the exchange rate: '))
-cost_per_all = amount * cost_per_one
-print("The total amount of dollars: {}".format(cost_per_all))
+amount = round(float(input()), 2)
+RUB = 13291.13
+ARS = 16294.55
+HNL = 4391.04
+AUD = 234.67
+MAD = 1620.35
+print("I will get {} RUB from the sale of {} litecoins.".format(round(amount * RUB, 2), amount))
+print("I will get {} ARS from the sale of {} litecoins.".format(round(amount * ARS, 2), amount))
+print("I will get {} HNL from the sale of {} litecoins.".format(round(amount * HNL, 2), amount))
+print("I will get {} AUD from the sale of {} litecoins.".format(round(amount * AUD, 2), amount))
+print("I will get {} MAD from the sale of {} litecoins.".format(round(amount * MAD, 2), amount))
