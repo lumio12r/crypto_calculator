dab9f0fc7c0e58f9c7abd656a0b663e244ba5c7e
lucas <lu.zatonski34@gmail.com>
2021-03-05 19:42:40 +0100

    Litecoin input from

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-amount = int(input())
-cost_per_one = 688
+amount = int(input('Please, enter the number of litecoins you have: '))
+cost_per_one = float(input('Please, enter the exchange rate: '))
 cost_per_all = amount * cost_per_one
-print("I have {} litecoin.".format(amount))
-print("{} litecoins cost {} dollars".format(amount, cost_per_all))
-print("I am rich! Yippee!")
+print("The total amount of dollars: {}".format(cost_per_all))
