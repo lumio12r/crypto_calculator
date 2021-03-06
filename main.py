5f7e56049eb6e96bb41e51476b0dba9aa07fae3e
lucas <lu.zatonski34@gmail.com>
2021-03-07 00:21:30 +0100

    Now, connection with api, convert to json and extracting data from dictionary

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-amount = round(float(input()), 2)
-RUB = 13291.13
-ARS = 16294.55
-HNL = 4391.04
-AUD = 234.67
-MAD = 1620.35
-print("I will get {} RUB from the sale of {} litecoins.".format(round(amount * RUB, 2), amount))
-print("I will get {} ARS from the sale of {} litecoins.".format(round(amount * ARS, 2), amount))
-print("I will get {} HNL from the sale of {} litecoins.".format(round(amount * HNL, 2), amount))
-print("I will get {} AUD from the sale of {} litecoins.".format(round(amount * AUD, 2), amount))
-print("I will get {} MAD from the sale of {} litecoins.".format(round(amount * MAD, 2), amount))
+import json
+import requests
+
+currency = input()
+
+r = requests.get('http://www.floatrates.com/daily/{}.json'.format(currency.lower()))
+open("currency.json", 'wb').write(r.content)
+with open("currency.json", "r") as file_json:
+    currency_from_json = json.load(file_json)
+print(currency_from_json['usd'])
+print(currency_from_json['eur'])
